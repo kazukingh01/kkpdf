821ca5e1d7356e658346bc41656cef800f6f3092
kazukingh01 <kazukingh01@gmail.com>
2025-01-30 19:53:32 +0900

    modify

--- main/img2pdf.py
+++ main/img2pdf.py
@@ -1,27 +1,33 @@
+import argparse
 from PIL import Image
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.utils import ImageReader
 
-# 画像ファイルのパス
-png_path = "../10-07.png"
-# 生成するPDFファイルのパス
-pdf_path = "../10-07.pdf"
 
-# 画像を開きます
-img = Image.open(png_path)
-
-# 画像をリサイズします。このステップは必要に応じてスキップしても構いません。
-# PDFのサイズ (ここではUS Letterサイズ) に合わせて画像をリサイズします。
-
-# ReportLabのキャンバスを作成します
-c = canvas.Canvas(pdf_path)
-
-# PIL画像をReportLabが使用できる画像に変換します
-img = ImageReader(img)
-
-# 画像をPDFに描画します
-c.drawImage(img, 0, 0, *letter)
-
-# PDFを保存します
-c.save()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, required=True)
+    args = parser.parse_args()
+    img = Image.open(args.path)
+    img_width, img_height = img.size
+    page_width, page_height = letter
+    scale = min(page_width / img_width, page_height / img_height)
+    new_width = img_width * scale
+    new_height = img_height * scale
+    c = canvas.Canvas(f"{args.path}.pdf", pagesize=letter)
+    c.setFillColorRGB(1, 1, 1)
+    c.rect(0, 0, page_width, page_height, fill=1, stroke=0)
+    x = (page_width - new_width) / 2
+    y = (page_height - new_height) / 2
+    c.drawImage(
+        ImageReader(img),
+        x,
+        y,
+        width=new_width,
+        height=new_height,
+        preserveAspectRatio=True,
+        mask='auto'
+    )
+    c.showPage()
+    c.save()
